4cf415b7c42250e877333cec94ceec80e5a46349
Robert Hogan <rhogan7@gmail.com>
2022-01-06 14:07:10 -0800

    First commit.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script context="module">
 	export const prerender = true;
+	import 'lazysizes';
 </script>
 
 <script>
@@ -11,49 +12,107 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
+	<h1>IMGIX configuration</h1>
+<div class="grid">
+	<div class="intro">
+		<div class="picture">
+			<h2>Picture Element</h2>
+			<figure>
+				<img 
+					src="https://vintagewineestates.imgix.net/common/images/banners/CPDawn2.jpg?auto=compress,formatenhance&fit=crop&h=400" 
+					alt=""
+				/>
+				<figcaption>An elephant at sunset</figcaption>
+			</figure>
+
+			<figure>
+				<img 
+					src="https://vintagewineestates.imgix.net//static/images/2017/brands/gw/gw-wine-and-cheese-750x550.jpg?auto=compress,formatenhance&fit=crop&h=400" 
+					alt=""
+				/>
+				<figcaption>An elephant at sunset</figcaption>
+			</figure>
 		</div>
+		<div class="srcset">
+			<h2>Img SrcSet</h2>
+			<figure>
+				<img 
+					src="https://vintagewineestates.imgix.net/common/images/banners/CPDawn2.jpg?auto=compress,formatenhance&fit=crop&h=400" 
+					alt=""
+				/>
+				<figcaption>An elephant at sunset</figcaption>
+			</figure>
+			<figure>
+				<img 
+					src="https://vintagewineestates.imgix.net//static/images/2017/brands/gw/gw-wine-and-cheese-750x550.jpg?auto=compress,formatenhance&fit=crop&h=400" 
+					alt=""
+				/>
+				<figcaption>An elephant at sunset</figcaption>
+			</figure>
+		</div>
+	</div>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<div class="lazyload">
+		<div class="picture">
+			<h2>Lazyload Picture Element</h2>
+			<figure>
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
+			<img 
+				src="https://vintagewineestates.imgix.net/common/images/banners/CPDawn2.jpg?auto=compress,formatenhance&fit=crop&h=400" 
+				alt=""
+			/>
+			<figcaption>An elephant at sunset</figcaption>
+		</figure>
+		<figure>
 
-	<Counter />
+			<img 
+				src="https://vintagewineestates.imgix.net//static/images/2017/brands/gw/gw-wine-and-cheese-750x550.jpg?auto=compress,formatenhance&fit=crop&h=400" 
+				alt=""
+			/>
+			<figcaption>An elephant at sunset</figcaption>
+		</figure>
+		</div>
+		<div class="srcset">
+			<h2>Lazyload Img SrcSet</h2>
+			<figure>
+
+			<img 
+			src="https://vintagewineestates.imgix.net//static/images/2017/brands/gw/gw-wine-and-cheese-750x550.jpg?auto=compress,formatenhance&fit=crop&h=400" 
+			alt=""
+			class="lazyload">
+			<figcaption>An elephant at sunset</figcaption>
+		</figure>
+		<figure>
+
+		<img 
+			class="lazyload"
+			data-src="https://vintagewineestates.imgix.net/common/images/banners/CPDawn2.jpg?auto=compress,format&fit=crop&crop=right&h=400" 
+			src="https://vintagewineestates.imgix.net/common/images/banners/CPDawn2.jpg?auto=compress,format&fit=crop&crop=right&h=400&px=16&blur=200"
+			alt="" />
+			<figcaption>An elephant at sunset</figcaption>
+		</figure>
+		</div>
+	</div>
+</div>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
+	.grid {
+		display: grid;
+		grid-template-columns: repeat( auto-fit, minmax(400px, 1fr) );
 	}
 
-	h1 {
-		width: 100%;
+	.intro {
+		height: 100vh;
 	}
 
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	h2 {
+		text-align: center;
 	}
 
-	.welcome img {
-		position: absolute;
+	img {
 		width: 100%;
-		height: 100%;
-		top: 0;
 		display: block;
+		max-width: 400px;
 	}
 </style>
